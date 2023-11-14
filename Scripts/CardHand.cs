db69216c6564b2e47cb4ccab5cdc5046cd8a5119
jkvastad <jkvastad@gmail.com>
2023-11-14 08:31:23 +0100

    Added tweening when sorting and moving cards

--- Scripts/CardHand.cs
+++ Scripts/CardHand.cs
@@ -3,12 +3,14 @@ using System;
 
 public partial class CardHand : Container
 {
+    [Export]
+    public float sortSpeed = 1.0f;
     Card selectedCard;
     Vector2 selectionOffset;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
-    {        
+    {
         SortChildren += SortCards;
         Godot.Collections.Array<Node> cards = GetChildren();
         foreach (Card card in cards)
@@ -16,14 +18,13 @@ public partial class CardHand : Container
             card.GuiInput += (InputEvent theEvent) => CardIsClicked(theEvent, card);
         }
     }
-    
 
     public override void _PhysicsProcess(double delta)
     {
         if (selectedCard != null)
         {
             selectedCard.Position = GetGlobalMousePosition() - selectionOffset;
-        }        
+        }
     }
 
     public void SortCards()
@@ -33,9 +34,12 @@ public partial class CardHand : Container
         foreach (Card card in cards)
         {
             Vector2 position = new Vector2(128f * i, 0f);
-            i++;                        
-            card.tween = CreateTween();            
-            card.tween.TweenProperty(card, "position", position, 0.5f);
+            i++;
+            if (card.tween == null || !card.tween.IsValid())
+            {
+                card.tween = CreateTween();
+                card.tween.TweenProperty(card, "position", position, sortSpeed);
+            }
         }
     }
 
@@ -47,12 +51,14 @@ public partial class CardHand : Container
             {
                 selectedCard = card;
                 selectionOffset = card.GetLocalMousePosition();
-                GD.Print("Card " + card.text.Text + " clicked with " + @event.AsText());                                
+                GD.Print("Card " + card.text.Text + " clicked with " + @event.AsText()); //TODO remove
+                card.tween?.Kill();
+
             }
             if (!mouseEvent.Pressed && mouseEvent.ButtonIndex == MouseButton.Left)
             {
                 selectedCard = null;
-                GD.Print("Card " + card.text.Text + " released with " + @event.AsText());
+                GD.Print("Card " + card.text.Text + " released with " + @event.AsText()); //TODO remove
                 EmitSignal(SignalName.SortChildren);
             }
         }
