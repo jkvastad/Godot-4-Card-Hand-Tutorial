da3bd252786703b15b4593e2239399c9f07000fb
jkvastad <jkvastad@gmail.com>
2023-11-18 11:58:27 +0100

    Added explanations for the code

--- Scripts/Card.cs
+++ Scripts/Card.cs
@@ -1,5 +1,4 @@
 using Godot;
-using System;
 
 public partial class Card : Area2D
 {
@@ -8,17 +7,11 @@ public partial class Card : Area2D
 	public Tween tween;
     [Export]
     public Sprite2D cardArt;
-
-	// Called when the node enters the scene tree for the first time.
+	
 	public override void _Ready()
 	{
 		textLabel.Text = GetPath();
 	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-    }
 
     public override void _MouseEnter()
     {        
